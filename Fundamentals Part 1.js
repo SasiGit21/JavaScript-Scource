aba733e84a9599525ab5f74ba538d82f8a87f247
SasiGit21 <sasikalaraj21@gmail.com>
2022-08-25 15:26:26 -0700

    First Commit

---
+++ Fundamentals Part 1.js
@@ -0,0 +1,108 @@
+/*let country = 'AndamanNicobarisland';
+const continent ='Asia';
+let populations = '100000000';
+console.log(continent,country,populations);
+
+let isLand =false;
+const language ='English';
+//const continent ='Europe'; // constant value canot be changed , let can be changed
+
+console.log(typeof(country));
+console.log(isLand);
+console.log(typeof(isLand));
+isLand = true;
+console.log(isLand);
+console.log(typeof(populations));
+console.log(typeof(language));
+console.log(typeof(continent));
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+console.log(javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+ year = 1191 ;
+ console.log(typeof year);
+ console.log(year);
+
+ console.log(typeof null);
+
+
+let age = 30;
+age = 31;
+
+//const can never change
+const bithday = 1991;
+//bithday =1990;
+
+//const job; // wont work without initilization
+
+var job ='programmer';
+job = 'teacher'
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas,ageSarah);
+
+console.log(ageJonas * 2 , ageJonas/10, 2**3);
+const firstName ='Jonas';
+const lastName = 'Schamr';
+console.log(firstName+' '+lastName);
+
+//Assiment operators
+let x = 10 +5;
+x += 10;
+x *=4;
+x++;
+x--;
+x--;
+console.log(x)
+
+//Comparision operatores
+console.log(ageJonas>ageSarah);
+console.log(ageSarah >= 18);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+
+console.log(now - 1991 > now -2018);
+
+*/
+
+const marks_mass = 78;
+const marks_height =1.69;
+const jons_mass=92 ;
+const jons_height = 1.95;
+ 
+
+ 
+const Marks_BMI = marks_mass/(marks_height**2);
+const Jons_BMI = jons_mass/(jons_height**2);
+console.log("Marks BMI "+Marks_BMI);
+console.log("Jons_BMI " + Jons_BMI);
+ markHigherBMI = Marks_BMI>Jons_BMI ;
+ console.log(  markHigherBMI);
+
+ const marks_mass1 = 95;
+const marks_height1 =1.88;
+const jons_mass1=85 ;
+const jons_height1 = 1.76;
+const Marks_BMI1 = marks_mass1/(marks_height1**2);
+const Jons_BMI1 = jons_mass1/(jons_height1**2);
+console.log("Marks BMI "+Marks_BMI1);
+console.log("Jons_BMI " + Jons_BMI1);
+markHigherBMI1 = Marks_BMI1>Jons_BMI1 ;
+ console.log(markHigherBMI1);
---
+++ script.js
@@ -0,0 +1,17 @@
+let js = 'amazing';
+console.log(40+8+23-10);
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Jonas';
+console.log(firstName);
+
+let firstName1 = "Matilda";
+
+console.log(firstName1);
+
+//variable contails number,$ , Underscore ,Alphabet
+
+//let jonas&meatilda = "JN";   /& is not allowed
+//let new = 27;          /new is a key word not alowed as variable
+//let Person ='jonas';   //Caps starting not allowed
